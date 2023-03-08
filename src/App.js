5b6a4628abaa9c1856c1671e5a4ecc2f6f961918
sakusanpuwan <sakusanpuwan98@gmail.com>
2023-03-08 13:32:00 +0000

    added .env

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
 import Logo from './assets/Logo.png'
 import DALLE from './components/DALLE/DALLE';
 import ChatGPT from './components/ChatGPT/ChatGPT';
+import Home from './components/Home';
+
+console.log(process.env);
 
 
 function App() {
@@ -10,14 +13,15 @@ function App() {
     <div className="App">
       <BrowserRouter>
 
-          <div className='App-Container'>
+          <div className='NavBar'>
             <h3><Link to='/alfred/dalle'>Alfred, paint me a picture....</Link></h3>
-            <img src={Logo} height={100}></img>
+            <Link to='/alfred'><img src={Logo} height="100px"/></Link>
             <h3><Link to='/alfred/chat'>Alfred, answer this question...</Link></h3>
           </div>
 
 
         <Routes>
+          <Route path='/alfred' element={<Home/>}/>
           <Route path='/alfred/dalle' element={<DALLE/>}/>
           <Route path='/alfred/chat' element={<ChatGPT/>}/>
         </Routes>
--- src/components/DALLE/DALLE.js
+++ src/components/DALLE/DALLE.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+
 
 const DALLE = () => {
+ 
+  console.log(process.env.REACT_APP_OPENAI_API_KEY);
+  
   return (
     <div className='DALLE-Container'>DALL-E</div>
   )
 }
 
-export default DALLE
+export default DALLE;
