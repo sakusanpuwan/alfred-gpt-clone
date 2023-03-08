46ad48e69e7b518aac2845965fe2bfa68c73409a
sakusanpuwan <sakusanpuwan98@gmail.com>
2023-03-08 17:20:38 +0000

    added image gen function successfully

--- src/components/DALLE/DALLE.js
+++ src/components/DALLE/DALLE.js
@@ -1,12 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './DALLE.css'
+const { Configuration, OpenAIApi } = require("openai");
+
+
 
 
 const DALLE = () => {
- 
-  console.log(process.env.REACT_APP_OPENAI_API_KEY);
-  
+
+  const [prompt,setPrompt] = useState("");
+  const [result,setResult] = useState("")
+
+  const configuration = new Configuration({
+    apiKey: process.env.REACT_APP_API_KEY,
+  });
+
+  const openai = new OpenAIApi(configuration);
+
+  const generateImage = async () => {
+
+    try {
+      const response = await openai.createImage({
+        prompt: prompt,
+        n:1,
+        size:"1024x1024"
+      });
+
+      const image_url = response.data.data[0].url;
+      console.log(image_url);
+      setResult(image_url)
+
+    } catch (error) {
+      console.log(error);
+    }
+
+  }
+
+
   return (
-    <div className='DALLE-Container'>DALL-E</div>
+    <div className='DALLE-Container'>
+      <h3>What would you like me to paint? 🎨</h3>
+      <input 
+        className='input' 
+        onChange={(event) => setPrompt(event.target.value)}
+        placeholder = "Type here..."
+      />
+      <button onClick={generateImage} className='button'>Click to paint</button>
+      {result.length > 0 ? <img src={result} alt='result image' className='result-image' /> : <></> }
+      
+    </div>
   )
 }
 
